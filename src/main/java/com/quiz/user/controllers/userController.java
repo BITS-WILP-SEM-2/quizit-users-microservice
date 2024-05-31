e7f955c50668d9faf6148428780bc2118b66174f
I528614 <shivang.agarwal@sap.com>
2024-05-31 17:56:55 +0530

    cors fix

--- src/main/java/com/quiz/user/controllers/userController.java
+++ src/main/java/com/quiz/user/controllers/userController.java
@@ -12,6 +12,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin
 public class userController {
 
 	@Autowired
